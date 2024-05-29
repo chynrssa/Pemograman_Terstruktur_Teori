33f58087d4fc0f22eedd0a1a8cbdd0fb655f5b19
K e y y <151293013+NabilaCahayaPutri@users.noreply.github.com>
2024-05-29 22:56:20 +0700

    Pemrograman Terstruktur

--- implementasi stack/Soal 3/Fungsi Precedence.cpp
+++ implementasi stack/Soal 3/Fungsi Precedence.cpp
@@ -6,3 +6,18 @@
 #include <string>
 
 using namespace std;
+
+bool Operator(char karakter) {
+    return karakter == '+' or karakter == '-' or karakter == '*' or karakter == '/' or 
+                        karakter == '%' or karakter == '(' or karakter == ')';
+}
+
+bool Digit(char karakter) {
+    return isdigit(karakter);
+}
+
+int Precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/' || op == '%') return 2;
+    return 0;
+}
